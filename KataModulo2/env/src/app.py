9531f10ca26eb9bccf0c797d6ee071a374f34cc9
noelsilis <noelsilis99@gmail.com>
2022-02-12 12:20:38 -0600

    Katas 0-6

---
+++ KataModulo2/env/src/app.py
@@ -0,0 +1,8 @@
+#Tema Kata: Crear y administrar proyectos
+
+from datetime import *
+from dateutil.relativedelta import *
+now = datetime.now()
+print(now)
+now = now + relativedelta(months=1, weeks=1, hour=10)
+print(now)
